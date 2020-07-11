f0d276ea662bf0ca8e767a68ac1cbcbea21145c5
wuyuxin511 <45875465+wuyuxin511@users.noreply.github.com>
2020-07-10 17:17:21 -0700

    Add unit tests to TemplateDataUtil class (#12)
    * Add unit tests to TemplateDataUtil class
    
    * Add more test cases
    
    * Improve code standarization
    
    Co-authored-by: Yuxin Wu <yuxinwu@google.com>

---
+++ src/test/java/tsunami/security/scanner/utilities/TemplateDataUtilTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tsunami.security.scanner.utilities;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.gson.JsonSyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class TemplateDataUtilTest {
+
+  @Test
+  public void parseTemplateData_whenInputValid_success() {
+    String templateDataJson =
+        "{'mysql_version':'5.6','password':'dfhieuwhfhdsfj','wordpress_version':'4.8-apache'}";
+
+    Map<String, String> resultMap = TemplateDataUtil.parseTemplateDataJson(templateDataJson);
+
+    assertThat(resultMap)
+        .containsExactly(
+            "mysql_version",
+            "5.6",
+            "password",
+            "dfhieuwhfhdsfj",
+            "wordpress_version",
+            "4.8-apache");
+  }
+
+  @Test
+  public void parseTemplateData_whenMapDataEmpty_success() {
+    String templateDataJson = "{}";
+
+    Map<String, String> resultMap = TemplateDataUtil.parseTemplateDataJson(templateDataJson);
+
+    assertThat(resultMap).isEmpty();
+  }
+
+  @Test
+  public void parseTemplateData_whenStringEmpty_failed() {
+    String templateDataJson = "{'mysql_version':''}";
+
+    Map<String, String> resultMap = TemplateDataUtil.parseTemplateDataJson(templateDataJson);
+
+    assertThat(resultMap).containsExactly("mysql_version", "");
+  }
+
+  @Test
+  public void parseTemplateData_whenJsonNotMap_failed() {
+    String templateDataJson = "{'mysql_version':['5.6','1.8']}";
+
+    assertThrows(
+        JsonSyntaxException.class, () -> TemplateDataUtil.parseTemplateDataJson(templateDataJson));
+  }
+}
