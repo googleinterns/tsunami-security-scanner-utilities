2957f1882d87151c7e2442d133f7ccccae9b182c
wuyuxin511 <45875465+wuyuxin511@users.noreply.github.com>
2020-07-14 16:04:28 -0700

    Add unit tests for App main (#11)
    * Add unit tests for App main
    
    * Improve code standarization, work of mock CoreV1Api still in process
    
    * Mock the Kubernetes Java Client Api
    
    * More precise assertion
    
    * small fixes
    
    * code standarization
    
    Co-authored-by: Yuxin Wu <yuxinwu@google.com>

--- src/main/java/tsunami/security/scanner/utilities/App.java
+++ src/main/java/tsunami/security/scanner/utilities/App.java
@@ -16,21 +16,28 @@
 
 package tsunami.security.scanner.utilities;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
+
 import com.beust.jcommander.JCommander;
 import com.google.common.io.Files;
+import freemarker.template.TemplateException;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
 import io.kubernetes.client.util.Config;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 public class App {
+  private final KubeJavaClientUtil kubeJavaClientUtil;
 
-  public static void main(String[] args) throws IOException, ApiException, ClassNotFoundException {
+  public App(KubeJavaClientUtil kubeJavaClientUtil) {
+    this.kubeJavaClientUtil = kubeJavaClientUtil;
+  }
 
+  public void run(String[] args) throws ApiException, TemplateException, IOException {
     // Parse args read from command line
     ApplicationArgs jArgs = new ApplicationArgs();
     JCommander helloCmd = JCommander.newBuilder().addObject(jArgs).build();
@@ -43,36 +50,41 @@ public class App {
     System.out.println(
         "App: " + appName + " Config Path: " + appConfigPath + " templateData: " + templateData);
 
+    // Set a default directory for configs if no config path is passed in.
+    if (isNullOrEmpty(appConfigPath)) {
+      appConfigPath = System.getProperty("user.dir") + "/application";
+    }
+
     // Combine the file path with application name as a directory.
     String configPath = appConfigPath + "/" + appName + "/";
 
     // Transform input template data Json String to Map.
     Map<String, String> templateDataMap = TemplateDataUtil.parseTemplateDataJson(templateData);
 
+    // Load all application's config files, run services and deploy the app on GKE.
+    File configFiles = new File(configPath);
+
+    // Check if configFiles is an existed directory
+    if (!configFiles.isDirectory()) throw new FileNotFoundException("Wrong directory.");
+
+    // Traverse all files under certain application's config path
+    for (File configFile : Files.fileTraverser().depthFirstPreOrder(configFiles)) {
+
+      if (configFile.isFile()) {
+        String resourceConfig = FreeMarkerUtil.replaceTemplates(templateDataMap, configFile);
+
+        // Parse all config to Kubernetes Objects and create them.
+        kubeJavaClientUtil.createResources(resourceConfig);
+      }
+    }
+  }
+
+  public static void main(String[] args) throws IOException, ApiException, TemplateException {
     // Initialize Kubernetes Java Client Api.
     ApiClient client = Config.defaultClient();
     Configuration.setDefaultApiClient(client);
 
-    // Load all application's config files, run services and deploy the app on GKE.
-    try {
-      File configFiles = new File(configPath);
-
-      // Traverse all files under certain application's config path
-      for (File configFile : Files.fileTraverser().depthFirstPreOrder(configFiles)) {
-
-        if (configFile.isFile()) {
-          String resourceConfig = FreeMarkerUtil.replaceTemplates(templateDataMap, configFile);
-
-          // Parse all config to Kubernetes Objects and create them.
-          KubeJavaClientUtil.createResources(resourceConfig);
-        }
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-
-    /* Example for creation of objects and deletion:
-     *   https://github.com/kubernetes-client/java/blob/master/examples/src/main/java/io/kubernetes/client/examples/YamlExample.java
-     */
+    App app = new App(new KubeJavaClientUtil());
+    app.run(args);
   }
 }
--- src/test/java/tsunami/security/scanner/utilities/AppTest.java
+++ src/test/java/tsunami/security/scanner/utilities/AppTest.java
@@ -16,12 +16,109 @@
 
 package tsunami.security.scanner.utilities;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.*;
 
-public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+import com.google.common.io.Files;
+import freemarker.template.TemplateException;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.apis.AppsV1Api;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Service;
+import io.kubernetes.client.util.Yaml;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+@RunWith(JUnit4.class)
+public final class AppTest {
+
+  private static final String RESOURCE_CONFIG =
+      "apiVersion: v1\n"
+          + "kind: Service\n"
+          + "metadata:\n"
+          + "  name: jupyter\n"
+          + "  labels:\n"
+          + "    app: jupyter\n"
+          + "spec:\n"
+          + "  ports:\n"
+          + "  - port: 80\n"
+          + "    name: http\n"
+          + "    targetPort: 8888\n"
+          + "  selector:\n"
+          + "    app: jupyter\n"
+          + "  type: LoadBalancer";
+  private App classUnderTest;
+
+  @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+  @Rule public TemporaryFolder folder = new TemporaryFolder();
+
+  @Mock CoreV1Api mockCoreV1Api;
+  @Mock AppsV1Api mockAppsV1Api;
+
+  @Before
+  public void setUp() {
+    this.classUnderTest = new App(new KubeJavaClientUtil(mockCoreV1Api, mockAppsV1Api));
+  }
+
+  @Test
+  public void runMain_whenInputValid_success() throws ApiException, IOException, TemplateException {
+    File jupyterFolder = folder.newFolder("jupyter");
+    File configFile = new File(jupyterFolder + "/jupyter.yaml");
+    Files.asCharSink(configFile, Charset.forName("UTF-8")).write(RESOURCE_CONFIG);
+
+    String[] args =
+        new String[] {
+          "--app",
+          "jupyter",
+          "--configPath",
+          folder.getRoot().getPath(),
+          "--templateData",
+          "{'jupyter_version':'notebook-6.0.3'}"
+        };
+
+    classUnderTest.run(args);
+
+    verify(mockCoreV1Api)
+        .createNamespacedService(
+            "default", (V1Service) Yaml.load(RESOURCE_CONFIG), null, null, null);
+  }
+
+  @Test
+  public void runMain_whenConfigPathIsMissing_success()
+      throws ApiException, IOException, TemplateException {
+    String[] args =
+        new String[] {"--app", "jupyter", "--templateData", "{'jupyter_version':'notebook-6.0.3'}"};
+
+    classUnderTest.run(args);
+
+    verify(mockCoreV1Api)
+        .createNamespacedService(
+            "default", (V1Service) Yaml.load(RESOURCE_CONFIG), null, null, null);
+  }
+
+  @Test
+  public void runMain_whenConfigPathDoesNotExist_failed() {
+    String[] args =
+        new String[] {
+          "--app",
+          "jupyter",
+          "--configPath",
+          folder.getRoot().getPath(),
+          "--templateData",
+          "{'jupyter_version':'notebook-6.0.3'}"
+        };
+
+    assertThrows(FileNotFoundException.class, () -> classUnderTest.run(args));
+  }
 }
